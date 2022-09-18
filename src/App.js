0c626fadf7ce45f6def945e806d649552dbb256d
Soumwadeep Guha <soumwadeep@gmail.com>
2022-09-18 11:11:19 +0530

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
-
+import "./App.css";
+import { Routes, Route, Navigate} from "react-router-dom";
+import NavBar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <h1>Welcome To Soumwadeep's Education System</h1>
+      <NavBar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      <Footer/>
     </>
   );
 }
