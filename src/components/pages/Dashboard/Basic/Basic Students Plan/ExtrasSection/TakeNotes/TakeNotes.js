bc188769c1d37f910c19fa0fee6d1e18227dd213
Soumwadeep Guha <soumwadeep@gmail.com>
2023-03-11 09:16:14 +0530

    Updated npm

--- src/components/pages/Dashboard/Basic/Basic Students Plan/ExtrasSection/TakeNotes/TakeNotes.js
+++ src/components/pages/Dashboard/Basic/Basic Students Plan/ExtrasSection/TakeNotes/TakeNotes.js
@@ -2,12 +2,9 @@ import React from 'react'
 
 const TakeNotes = () => {
   return (
-    <section id='TakeNotes'>
-        <h1>Take <span>Notes</span></h1>
-        <h3>
-          Work In Progress!
-        </h3>
-    </section>
+    <div>
+      
+    </div>
   )
 }
 
