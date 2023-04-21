70eadac43dec50dc14d2a8691e3be5509a1b63da
Soumwadeep Guha <soumwadeep@gmail.com>
2023-04-21 11:15:42 +0530

    Added Calculator Link New One

--- src/components/pages/Dashboard/Basic/Basic Students Plan/ExtrasSection/Calculator/Calculator.js
+++ src/components/pages/Dashboard/Basic/Basic Students Plan/ExtrasSection/Calculator/Calculator.js
@@ -48,7 +48,7 @@ const Calculator = () => {
         And Easy To Use Calculator.
       </p>
       <iframe
-        src="https://soumwadeep.github.io/EduSys/ScientificCalculator/"
+        src="https://edusys.co.in/ScientificCalculator/index.html"
         className="calciframe"
       ></iframe>
     </section>
